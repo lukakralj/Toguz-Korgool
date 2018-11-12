cad01aa38d130d08b62babe11dd3bd3e2c177062
k1764125 <luka.kralj@kcl.ac.uk>
2018-11-12 01:22:10 +0000

    Add empty test file.

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+/**
+ * Class that contains tests for Board.java.
+ *
+ * @author Luka Kralj
+ * @version 12 November 2018
+ */
+public class BoardTest {
+}
