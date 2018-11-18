7ceca695104f474f0be6eaef0aaf441549b439d1
k1764125 <luka.kralj@kcl.ac.uk>
2018-11-18 11:43:20 +0000

    Add color setter and comments

--- src/main/java/OvalButton.java
+++ src/main/java/OvalButton.java
@@ -10,36 +10,101 @@ public class OvalButton extends JButton {
     private Color colorBorderNormal;
     private Color colorBorderHighlighted;
 
+    /**
+     * Construct a default oval button.
+     */
     public OvalButton() {
         this("");
     }
 
+    /**
+     * Construct an oval button with this text.
+     *
+     * @param text Text to display.
+     */
     public OvalButton(String text) {
+        this(text, Color.WHITE, Color.LIGHT_GRAY, Color.BLACK, Color.RED);
+    }
+
+    /**
+     * Construct an oval button and specify its custom colour.
+     *
+     * @param text Text to display.
+     * @param colorNormal The main color of the button.
+     * @param colorHighlighted The color of the button when it is highlighted (hovered over etc.).
+     * @param colorBorderNormal The main border color of the button.
+     * @param colorBorderHighlighted The border color that will show whenever the button is marked as being
+     *                               highlighted (will remain this color even after the mouse exits).
+     */
+    public OvalButton(String text, Color colorNormal, Color colorHighlighted, Color colorBorderNormal, Color colorBorderHighlighted) {
         super(text);
-        colorNormal = Color.WHITE;
+        this.colorNormal = colorNormal;
+        this.colorHighlighted = colorHighlighted;
+        this.colorBorderNormal = colorBorderNormal;
+        this.colorBorderHighlighted = colorBorderHighlighted;
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        Dimension d = getSize();
-
-        g.fillOval(0, 0, d.width, d.height);
-        g.setColor(colorNormal);
+    /**
+     * Set the main color of the button.
+     *
+     * @param colorNormal
+     */
+    public void setColorNormal(Color colorNormal) {
+        this.colorNormal = colorNormal;
     }
 
+    /**
+     * Set the color of the button when it is highlighted (hovered over etc.).
+     *
+     * @param colorHighlighted
+     */
+    public void setColorHighlighted(Color colorHighlighted) {
+        this.colorHighlighted = colorHighlighted;
+    }
+
+    /**
+     * Set the main border color of the button.
+     *
+     * @param colorBorderNormal
+     */
+    public void setColorBorderNormal(Color colorBorderNormal) {
+        this.colorBorderNormal = colorBorderNormal;
+    }
+
+    /**
+     * Set the border color that will show whenever the button is marked as being
+     * highlighted (will remain this color even after the mouse exits).
+     *
+     * @param colorBorderHighlighted
+     */
+    public void setColorBorderHighlighted(Color colorBorderHighlighted) {
+        this.colorBorderHighlighted = colorBorderHighlighted;
+    }
+
+    /**
+     * This method is overridden because it adds additional checks before executing the
+     * action specified by the user of the button.
+     *
+     * @param l ActionListener specified by the user.
+     */
     @Override
     public void addActionListener(ActionListener l) {
         super.addActionListener(e -> {
             Point p = MouseInfo.getPointerInfo().getLocation();
-            if (isInRange(p.x, p.y)) {
+            if (isInOval(p.x, p.y)) {
                 l.actionPerformed(e);
             }
         });
     }
 
-    private boolean isInRange(int x, int y) {
+    /**
+     * Check if the specified point is within the oval of the button.
+     *
+     * @param x Coordinate x of the point we want to check.
+     * @param y Coordinate y of the point we want to check.
+     * @return True if the point is within the borders or on the border of the oval, false if it is outside of it.
+     */
+    private boolean isInOval(int x, int y) {
         // Calculate centre of the ellipse.
         double s1 = getLocationOnScreen().x + getSize().width / 2;
         double s2 = getLocationOnScreen().y + getSize().height / 2;
@@ -50,7 +115,20 @@ public class OvalButton extends JButton {
 
         // Check if the given point is withing the specified ellipse:
         return ((((x - s1)*(x - s1)) / (a*a)) + (((y - s2)*(y - s2)) / (b*b))) <= 1;
+    }
 
+    /**
+     * Takes care of rendering the oval button correctly.
+     *
+     * @param g
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Dimension d = getSize();
+        g.setColor(colorNormal);
+        g.fillOval(0, 0, d.width, d.height);
 
     }
 
